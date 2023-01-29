da7dc2d69c8b2aa1e7afb4f45d6cf4c43058d40d
Carol Choi <saekimidori@gmail.com>
2023-01-28 23:19:23 -0500

    ignore index.html

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const addNote = document.querySelector('#addNote')
-const form = document.querySelector('#form')
-
-addNote.addEventListener('click', newNote)
-
-function newNote() {
-        form.classList.toggle('hidden')
-}
