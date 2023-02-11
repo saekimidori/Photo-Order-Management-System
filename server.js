25e84741afe3dbd2760b6ba6456a9e29867edf0e
Carol Choi <saekimidori@gmail.com>
2023-02-11 11:58:48 -0500

    added bodyParser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express') // imports express
 const app = express() // assigns express to app variable
 const connectDB = require('./config/database')
 const methodOverride = require("method-override")
+const bodyParser= require('body-parser')
 const homeRoutes = require('./routes/home')
 const workspaceRoutes = require('./routes/workspace')
 // const MongoClient = require('mongodb').MongoClient // imports MongoDB client
@@ -24,6 +25,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 
 //Use forms for put / delete
