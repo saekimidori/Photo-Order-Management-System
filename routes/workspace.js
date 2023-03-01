b8e287c7a5e99608058fbea11e3d470af296884d
Carol Choi <saekimidori@gmail.com>
2023-02-28 22:09:31 -0500

    removed unnecessary code

--- routes/workspace.js
+++ routes/workspace.js
@@ -18,14 +18,4 @@ router.get('/delete/:id', workspaceController.deleteNote)
 
 router.post('/customer/update/:id', workspaceController.updateCustomer)
 
-// Retrieve a single Note with noteId
-// router.get('/:id', workspaceController.findOne);
-
-// Update a Note with noteId
-// router.put('/:noteId/update', workspaceController.updateNote);
-
-
-// router.put('/markIncomplete', workspaceController.markIncomplete)
-
-
 module.exports = router
