dc7fd6219ce3a0eb308dfd3130d1da4424939333
Carol Choi <saekimidori@gmail.com>
2023-03-05 23:27:51 -0500

    removed unnecessary code

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,5 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex) 
-// router.get('/workspace', homeController.getWorkspace) 
 
 module.exports = router
