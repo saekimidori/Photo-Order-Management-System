e4658ca5e5acd9d31eb68e115f0be6c60e9070c0
Carol Choi <saekimidori@gmail.com>
2023-03-08 23:35:39 -0500

    moved customer controllers & routes back to workspace conntroller/routes. reroute from new customer submission not working

--- routes/workspace.js
+++ routes/workspace.js
@@ -12,6 +12,11 @@ router.post('/markResolved/:id', workspaceController.markResolved)
 router.post('/update/:id', workspaceController.updateNote)
 router.get('/delete/:id', workspaceController.deleteNote)
 
+// customer routes
+// ideally, this should not be here
 router.get('/customer/:id', workspaceController.getCustomer)
+router.get('/customer/:id/edit', workspaceController.getCustomerEdit)
+router.post('/newCustomer', workspaceController.newCustomer)
+router.post('/customer/update/:id', workspaceController.updateCustomer)
 
 module.exports = router
