f2243e64d3d35bb8c13a858a0d4e428a63d38c3f
Carol Choi <saekimidori@gmail.com>
2023-04-25 22:10:43 -0400

    added User model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+mongoose.set("strictQuery", false);
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('product', UserSchema) // first argument is database name. automatically turns plural in MongoDB
