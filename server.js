58460521c29a5d95f2b22770ad8ac476d0adedfb
Carol Choi <saekimidori@gmail.com>
2023-05-02 12:32:21 -0400

    organized and added comments. added alternative for passport auth

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express = require('express') // imports express
 const app = express() // assigns express to app variable
 const PORT = 5000 // port is assigned to 5000
-const mongoose = require("mongoose")
+const mongoose = require("mongoose") // imports mongoose
 const passport = require('passport') // imports passport for authentication
-const LocalStrategy = require('passport-local');
-const session = require('express-session');
+const session = require('express-session') // allows for user sessions
+const MongoStore = require('connect-mongo')(session) // stores user sessions in the database
+const flash = require('express-flash') // shows error messages for validation during authentication
+const logger = require('morgan') // logs error messages during authentication
+// const LocalStrategy = require('passport-local');
 // const crypto = require('crypto');
 
 // const strategy = new LocalStrategy(function verify(username, password, cb) {
@@ -23,7 +26,7 @@ const session = require('express-session');
 //   });
 // });
 
-// imports functions/routes
+// imports database connection and routes
 const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const workspaceRoutes = require('./routes/workspace')
@@ -36,6 +39,9 @@ const bodyParser= require('body-parser')
 
 // loads environment variables
 require('dotenv').config({path: './config/.env'}) 
+
+// Passport config
+require('./config/passport')(passport)
 
 // connect to database
 connectDB()
@@ -55,6 +61,33 @@ connectDB()
 // middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+// required to properly parse form POST requests - sending data; allows to read requests
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// use forms for put / delete
+app.use(methodOverride("_method"))
+
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+
 // // passport authenticates request
 // app.post('/login/password',
 //   passport.authenticate('local', { failureRedirect: '/login', failureMessage: true }),
@@ -84,19 +117,6 @@ app.use(express.static('public'))
 //     });
 // });
 
-// required to properly parse form POST requests - sending data; allows to read requests
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-// use forms for put / delete
-app.use(methodOverride("_method"))
-
-// Routes
-app.use('/', homeRoutes)
-app.use('/workspace', workspaceRoutes)
 // // renders login page to authenticate user
 // app.get('/login',
 //   function(req, res, next) {
@@ -108,6 +128,10 @@ app.use('/workspace', workspaceRoutes)
 //   function(req, res) {
 //     res.redirect('/~' + req.user.username);
 //   });
+
+// Routes
+app.use('/', homeRoutes)
+app.use('/workspace', workspaceRoutes)
 
 // start server
 app.listen(process.env.PORT || PORT, ()=>{ // server tries to load on environment variable first; if not, then it will load on assigned port above (|| PORT)
