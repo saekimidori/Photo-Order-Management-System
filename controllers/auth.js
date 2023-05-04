244ddbc44e0a18f97f64e75c3f7ef88b564615ff
Carol Choi <saekimidori@gmail.com>
2023-05-04 09:05:01 -0400

    removed email validators and fixed redirect to workspace

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 
 exports.getLogin = (req, res) => {
     if (req.user) {
-      return res.redirect('/todos')
+      return res.redirect('/workspace')
     }
     res.render('login', {
       title: 'Login'
@@ -13,14 +13,13 @@ exports.getLogin = (req, res) => {
 
 exports.postLogin = (req, res, next) => {
     const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+    if (validator.isEmpty(req.body.username)) validationErrors.push({ msg: 'Please enter a valid username.' })
     if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
   
     if (validationErrors.length) {
       req.flash('errors', validationErrors)
       return res.redirect('/login')
     }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
   
     passport.authenticate('local', (err, user, info) => {
       if (err) { return next(err) }
@@ -31,7 +30,7 @@ exports.postLogin = (req, res, next) => {
       req.logIn(user, (err) => {
         if (err) { return next(err) }
         req.flash('success', { msg: 'Success! You are logged in.' })
-        res.redirect(req.session.returnTo || '/todos')
+        res.redirect(req.session.returnTo || '/workspace')
       })
     })(req, res, next)
   }
@@ -49,7 +48,7 @@ exports.postLogin = (req, res, next) => {
   
   exports.getSignup = (req, res) => {
     if (req.user) {
-      return res.redirect('/todos')
+      return res.redirect('/workspace')
     }
     res.render('signup', {
       title: 'Create Account'
@@ -58,7 +57,7 @@ exports.postLogin = (req, res, next) => {
   
   exports.postSignup = (req, res, next) => {
     const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+    if (validator.isEmpty(req.body.username)) validationErrors.push({ msg: 'Please enter a valid username.' })
     if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
     if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
   
@@ -66,21 +65,16 @@ exports.postLogin = (req, res, next) => {
       req.flash('errors', validationErrors)
       return res.redirect('../signup')
     }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
   
     const user = new User({
-      userName: req.body.userName,
-      email: req.body.email,
+      username: req.body.username,
       password: req.body.password
     })
   
-    User.findOne({$or: [
-      {email: req.body.email},
-      {userName: req.body.userName}
-    ]}, (err, existingUser) => {
+    User.findOne({userName: req.body.userName}, (err, existingUser) => {
       if (err) { return next(err) }
       if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address or username already exists.' })
+        req.flash('errors', { msg: 'Username already exists.' })
         return res.redirect('../signup')
       }
       user.save((err) => {
@@ -89,7 +83,7 @@ exports.postLogin = (req, res, next) => {
           if (err) {
             return next(err)
           }
-          res.redirect('/todos')
+          res.redirect('/workspace')
         })
       })
     })
