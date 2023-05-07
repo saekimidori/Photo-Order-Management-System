4eadf70027bb3c3ab68d20003cfe9901e388c63b
Carol Choi <saekimidori@gmail.com>
2023-05-07 18:25:20 -0400

    added hash middleware and password validation method

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 mongoose.set("strictQuery", false);
@@ -16,8 +17,30 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     default: 'Basic',
-    required: true,
+    required: false,
   }
 })
+
+// Password hash middleware
+ UserSchema.pre('save', function save(next) {
+  const user = this
+  if (!user.isModified('password')) { return next() }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) { return next(err) }
+      user.password = hash
+      next()
+    })
+  })
+})
+
+
+// Helper method for validating user's password
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch)
+  })
+}
 
 module.exports = mongoose.model('user', UserSchema) // first argument is database name. automatically turns plural in MongoDB
